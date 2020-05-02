3183f19a9fbbc10494574876bced78ef11b51866
L <2033181465@qq.com>
2020-05-02 22:16:19 +0800

    完成定义接口请求函数

--- src/api/index.js
+++ src/api/index.js
@@ -11,9 +11,16 @@ export const requestCategorys=() => ajax (BASE_URL + `/index_category`)
 // 3、根据经纬度获取商铺列表
 export const requestShops = (latitude, longitude) => ajax(BASE_URL + '/shops', {latitude, longitude})
 // 4、根据经纬度和关键字搜索商铺列表
+export const requestSearchShops = (geohash,keyword) =>ajax(BASE_URL+'/search_shops',{geohash,keyword})
 // 5、获取一次性验证码
+
 // 6、用户名密码登陆
+export  const requestPwdLogin = (name,pwd,captcha,BASE_URL+"/login_pwd",{name,pwd,captcha},"POST")
 // 7、发送短信验证码
+export const requestSendCode = phone => ajax(BASE_URL+'/sendCode',{phone})
 // 8、手机号验证码登陆
+export const requestSmsLogin = (phone,code) =>ajax(BASE_URL+'/login_sms',{phone,code},"POST")
 // 9、根据会话获取用户信息
+export const requestUserInfo = () =>ajax(BASE_URL+'/userinfo')
 // 10、用户登出
+export  const requestLogout = () =>ajax(BASE_URL+'/logout')
