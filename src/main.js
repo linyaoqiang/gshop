caeb6f25a961eb2823e2064ffcc8cdc2504706fa
L <2033181465@qq.com>
2020-05-06 00:30:23 +0800

    使用vue-lazyload实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import router from './router/index'
 import store from './store/index'
 import {Button} from  'mint-ui'
 import './mock/mockServer'
+import VueLazyload from 'vue-lazyload'
+import loading from './common/imgs/loading.gif'
 
 /*注册为全局组件*/
 Vue.component(Button.name,Button)
+
+//使用图片懒加载
+Vue.use(VueLazyload,{
+  loading
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
