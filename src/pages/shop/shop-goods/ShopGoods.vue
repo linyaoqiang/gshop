4eff50946a91ef9be762490c2c1614171d2b7f63
L <2033181465@qq.com>
2020-05-05 14:03:38 +0800

    点击左侧菜单，更新右侧列表位置 滑动右侧列表时，更新左侧菜单状态

--- src/pages/shop/shop-goods/ShopGoods.vue
+++ src/pages/shop/shop-goods/ShopGoods.vue
@@ -4,7 +4,7 @@
       <ul>
         <!--current-->
         <li class="menu-item" v-for="(good, index) in goods" :key="index"
-            :class="{current: index===currentIndex}">
+            :class="{current: index===currentIndex}" @click.prevent="clickMenuItem(index)">
             <span class="text bottom-border-1px">
               <img class="icon" :src="good.icon" v-if="good.icon">
               {{good.name}}
@@ -45,21 +45,69 @@
 </template>
 
 <script>
-  import BScroll from  'better-scroll'
+  import BScroll from 'better-scroll'
   import {mapState} from 'vuex'
 
   export default {
     name: "ShopGoods",
+    data () {
+      return {
+        scrollY: 0, //当前右侧列表的top的值
+        tops: []  //每一个右侧列表li的top值
+      }
+    },
     computed: {
-      ...mapState(['goods'])
+      ...mapState(['goods']),
+      currentIndex () {
+        return this.tops.findIndex((top, index) => {
+          return top <= this.scrollY && this.tops[index + 1] > this.scrollY
+        })
+      }
     },
     mounted () {
-      this.$store.dispatch('getShopGoods',()=>{
-        this.$nextTick(()=>{
-          new BScroll(".menu-wrapper")
-          new BScroll('.foods-wrapper')
-        })
+      this.$store.dispatch('getShopGoods', () => {
+        this.$nextTick(this._initScroll)
       })
+    },
+    methods: {
+      _initScroll () {
+        new BScroll(".menu-wrapper", {
+          click: true
+        })
+        this.foodsScroll = new BScroll('.foods-wrapper', {
+          probeType: 2,
+          click: true
+        })
+
+        this.foodsScroll.on('scroll', ({x, y}) => {
+          this.scrollY = Math.abs(y)
+        })
+        this.foodsScroll.on('scrollEnd', ({x, y}) => {
+          this.scrollY = Math.abs(y)
+        })
+
+        this._initTops()
+      },
+      _initTops () {
+        let tops = []
+        let lis = document.querySelectorAll(".food-list-hook");
+        let top = 0;
+        tops.push(top)
+        Array.prototype.slice.call(lis).forEach(li => {
+          top += li.clientHeight
+          tops.push(top)
+        })
+        this.tops = tops;
+      },
+      clickMenuItem (index) {
+        //获取到左侧分类列表中的数据
+        const scrollY = this.tops[index]
+        //立即更新右侧状态
+        this.scrollY = scrollY
+        //右侧滚动到指定位置
+        this.foodsScroll.scrollTo(0,-scrollY,300)
+
+      }
     }
   }
 </script>
@@ -74,16 +122,19 @@
     width: 100%
     background: #fff;
     overflow: hidden
+
     .menu-wrapper
       flex: 0 0 80px
       width: 80px
       background: #f3f5f7
+
       .menu-item
         display: table
         height: 54px
         width: 56px
         padding: 0 12px
         line-height: 14px
+
         &.current
           position: relative
           z-index: 10
@@ -91,8 +142,10 @@
           background: #fff
           color: $green
           font-weight: 700
+
           .text
             border-none()
+
         .icon
           display: inline-block
           vertical-align: top
@@ -101,14 +154,17 @@
           margin-right: 2px
           background-size: 12px 12px
           background-repeat: no-repeat
+
         .text
           display: table-cell
           width: 56px
           vertical-align: middle
           bottom-border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
+
     .foods-wrapper
       flex: 1
+
       .title
         padding-left: 14px
         height: 26px
@@ -117,46 +173,58 @@
         font-size: 12px
         color: rgb(147, 153, 159)
         background: #f3f5f7
+
       .food-item
         display: flex
         margin: 18px
         padding-bottom: 18px
         bottom-border-1px(rgba(7, 17, 27, 0.1))
+
         &:last-child
           border-none()
           margin-bottom: 0
+
         .icon
           flex: 0 0 57px
           margin-right: 10px
+
         .content
           flex: 1
+
           .name
             margin: 2px 0 8px 0
             height: 14px
             line-height: 14px
             font-size: 14px
             color: rgb(7, 17, 27)
+
           .desc, .extra
             line-height: 10px
             font-size: 10px
             color: rgb(147, 153, 159)
+
           .desc
             line-height: 12px
             margin-bottom: 8px
+
           .extra
             .count
               margin-right: 12px
+
           .price
             font-weight: 700
             line-height: 24px
+
             .now
               margin-right: 8px
               font-size: 14px
               color: rgb(240, 20, 20)
+
             .old
               text-decoration: line-through
               font-size: 10px
               color: rgb(147, 153, 159)
+
           .cartcontrol-wrapper
             position: absolute
             right: 0
