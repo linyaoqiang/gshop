fd843084204a515842a8fbf030f0f0766c67f1ba
L <2033181465@qq.com>
2020-05-06 00:19:46 +0800

    组件懒加载，其代码将在第一次运行时通过请求加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../pages/home/Home.vue'
+
+
+/*import Home from '../pages/home/Home.vue'
 import Search from '../pages/search/Search.vue'
 import Order from '../pages/order/Order.vue'
-import Profile from '../pages/profile/Profile.vue'
+import Profile from '../pages/profile/Profile.vue'*/
 import Login from '../pages/login/Login'
 import Shop from  '../pages/shop/Shop'
+
+
+//用于实现懒加载  ，其会不包含在整个路由的js中，当需要用到时，就会发送请求获取对应路由组件的代码
+const Home = ()=>import('../pages/home/Home.vue')
+const Search = ()=>import('../pages/search/Search.vue')
+const Order  = ()=>import('../pages/order/Order.vue')
+const Profile = ()=>import('../pages/profile/Profile.vue')
+
+
 import ShopGoods from '../pages/shop/shop-goods/ShopGoods'
 import ShopRatings from  '../pages/shop/shop-ratings/ShopRating'
 import ShopInfo from '../pages/shop/shop-info/ShopInfo'
