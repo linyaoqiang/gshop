51acddd6706a9873e1fa87f5b9bf90dac88b6b65
L <2033181465@qq.com>
2020-05-05 16:19:41 +0800

    解决购物车商品列表的显示的bug，必须在有商品的情况下才能打开 没有商品隐藏，并且没有商品时，不能改变是否展示的变量

--- src/components/ShopCart/ShopCart.vue
+++ src/components/ShopCart/ShopCart.vue
@@ -43,14 +43,18 @@
 </template>
 
 <script>
+  import CartControl from '../CartControl/CartControl'
   import {mapState, mapGetters} from 'vuex'
 
   export default {
     name: "ShopCart",
     data () {
       return {
-        listShow: false
+        isShow: false
       }
+    },
+    components: {
+      CartControl
     },
     computed: {
       ...mapState(['cartFoods', 'info']),
@@ -62,15 +66,25 @@
         if (this.totalPrice == 0) {
           return `配送费￥${this.info.minPrice}`
         } else if (this.totalPrice < this.info.minPrice) {
-          return `还需要￥${this.info.minPrice-this.totalPrice}`
-        }else{
+          return `还需要￥${this.info.minPrice - this.totalPrice}`
+        } else {
           return '结算'
         }
+      },
+      listShow () {
+        if (this.totalCount === 0) {
+          this.isShow=false
+          return false
+        }
+        return this.isShow
       }
     },
     methods: {
+
       toggleShow () {
-        this.listShow = !this.listShow
+        if(this.totalCount>0){
+          this.isShow=!this.isShow
+        }
       },
       clearCart () {
 
@@ -89,13 +103,16 @@
     z-index 50
     width 100%
     height 48px
+
     .content
       display flex
       background #141d27
       font-size 0
       color rgba(255, 255, 255, 0.4)
+
       .content-left
         flex 1
+
         .logo-wrapper
           display inline-block
           vertical-align top
@@ -108,20 +125,25 @@
           box-sizing border-box
           border-radius 50%
           background #141d27
+
           .logo
             width 100%
             height 100%
             border-radius 50%
             text-align center
             background #2b343c
+
             &.highlight
               background $green
+
             .icon-shopping_cart
               line-height 44px
               font-size 24px
               color #80858a
+
               &.highlight
                 color #fff
+
           .num
             position absolute
             top 0
@@ -136,6 +158,7 @@
             color #ffffff
             background rgb(240, 20, 20)
             box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.4)
+
         .price
           display inline-block
           vertical-align top
@@ -146,17 +169,21 @@
           font-size 16px
           font-weight 700
           color #fff
+
           &.highlight
             color #fff
+
         .desc
           display inline-block
           vertical-align bottom
           margin-bottom 15px
           margin-left -45px
           font-size 10px
+
       .content-right
         flex 0 0 105px
         width 105px
+
         .pay
           height 48px
           line-height 48px
@@ -164,11 +191,14 @@
           font-size 12px
           font-weight 700
           color #fff
+
           &.not-enough
             background #2b333b
+
           &.enough
             background #00b43c
             color #fff
+
     .ball-container
       .ball
         position fixed
@@ -176,12 +206,14 @@
         bottom 22px
         z-index 200
         transition all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+
         .inner
           width 16px
           height 16px
           border-radius 50%
           background $green
           transition all 0.4s linear
+
     .shopcart-list
       position absolute
       left 0
@@ -189,20 +221,25 @@
       z-index -1
       width 100%
       transform translateY(-100%)
+
       &.move-enter-active, &.move-leave-active
         transition transform .3s
+
       &.move-enter, &.move-leave-to
         transform translateY(0)
+
       .list-header
         height 40px
         line-height 40px
         padding 0 18px
         background #f3f5f7
         border-bottom 1px solid rgba(7, 17, 27, 0.1)
+
         .title
           float left
           font-size 14px
           color rgb(7, 17, 27)
+
         .empty
           float right
           font-size 12px
@@ -213,15 +250,18 @@
         max-height 217px
         overflow hidden
         background #fff
+
         .food
           position relative
           padding 12px 0
           box-sizing border-box
           bottom-border-1px(rgba(7, 17, 27, 0.1))
+
           .name
             line-height 24px
             font-size 14px
             color rgb(7, 17, 27)
+
           .price
             position absolute
             right 90px
@@ -230,6 +270,7 @@
             font-size 14px
             font-weight 700
             color rgb(240, 20, 20)
+
           .cartcontrol-wrapper
             position absolute
             right 0
@@ -245,8 +286,10 @@
     backdrop-filter blur(10px)
     opacity 1
     background rgba(7, 17, 27, 0.6)
+
     &.fade-enter-active, &.fade-leave-active
       transition all 0.5s
+
     &.fade-enter, &.fade-leave-to
       opacity 0
       background rgba(7, 17, 27, 0)
